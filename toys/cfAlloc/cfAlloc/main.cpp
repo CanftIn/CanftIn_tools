6dbfc0f8b626580ade855dcdc5464728999bd73c
CanftIn <wwc7033@gmail.com>
2018-02-05 18:12:11 +0800

    update

---
+++ toys/cfAlloc/cfAlloc/main.cpp
@@ -0,0 +1,17 @@
+#include "cfAlloc.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int ia[5] = { 0, 1, 2, 3, 4 };
+	unsigned int i;
+	vector<int, CF::allocator<int>> iv(ia, ia + 5);
+	for (i = 0; i < iv.size(); i++)
+	{
+		cout << iv[i] << ' ';
+	}
+	cout << endl;
+}

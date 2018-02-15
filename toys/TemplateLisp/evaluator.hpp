fe7c5fc723c20cee2e7c219b6d75f89aaa11b7a8
CanftIn <wwc7033@gmail.com>
2018-02-15 22:28:41 +0800

    update List

--- toys/TemplateLisp/evaluator.hpp
+++ toys/TemplateLisp/evaluator.hpp
@@ -8,41 +8,78 @@ namespace CAN
     namespace TL
     {
         using namespace CAN::TL::UTIL;
+
+        // Type of data
         template<int N> struct Int;
         template<bool B> struct Bool;
+        struct Unit;
 
-        //Add
+        // Add
         template<class T1, class T2> struct Add;
-
         template<int N1, int N2>
         struct Add<Int<N1>, Int<N2>>
         {
             using value = Int<(N1 + N2)>;
         };
 
-        //Compare
+        // Compare
+        // >
         template<class T1, class T2> struct IsGreater;
-
         template<int N1, int N2>
         struct IsGreater<Int<N1>, Int<N2>>
         {
             using value = Bool<(N1 > N2)>;
         };
 
+        // <
         template<class T1, class T2> struct IsLess;
-
         template<int N1, int N2>
         struct IsLess<Int<N1>, Int<N2>>
         {
             using value = Bool<(N1 < N2)>;
         };
 
+        // ==
         template<class T1, class T2> struct IsEqual;
-
         template<int N1, int N2>
         struct IsEqual<Int<N1>, Int<N2>>
         {
             using value = Bool<(N1 == N2)>;
+        };
+
+        // Pair
+        template<class T1, class T2> struct Pair;
+
+        // Pair.first
+        template<class T> struct First;
+        template<class T1, class T2>
+        struct First<Pair<T1, T2>>
+        {
+            using value = T1;
+        };
+
+        // Pair.second
+        template<class T> struct Second;
+        template<class T1, class T2>
+        struct Second<Pair<T1, T2>>
+        {
+            using value = T2;
+        };
+
+        // List
+        template<class T, class... T_Other> struct List;
+
+        // List.N
+        template<class T, class N> struct List_Ref;
+        template<class T, class... T_Other, int N>
+        struct List_Ref<List<T, T_Other...>, Int<N>>
+        {
+            using value = typename List_Ref<List<T_Other...>, Int<N-1>>::value;
+        };
+        template<class T, class... T_Other>
+        struct List_Ref<List<T, T_Other...>, Int<0>>
+        {
+            using value = T;
         };
     }
 }
--- toys/TemplateLisp/evaluator_test.cpp
+++ toys/TemplateLisp/evaluator_test.cpp
@@ -16,6 +16,25 @@ int main()
     StaticCheckEQ< IsLess< Int<5>, Int<8> >::value, Bool<true> >();
     StaticCheckEQ< IsEqual< Int<8>, Int<8> >::value, Bool<true> >();
 
+    // Pair Testing
+    using P = Pair< Pair<Int<4>, Bool<true>>,
+                    Pair<Pair<Int<2>, Unit>,
+                         Bool<false>>>;
+    StaticCheckEQ< First< First<P>::value >::value, Int<4> >();
+    StaticCheckEQ< Second< First< Second<P>::value >::value >::value, Unit >();
+    StaticCheckEQ< Second< Second<P>::value >::value, Bool<false> >();
 
+    // List
+    StaticCheckEQ< List_Ref< List< Int<0>, Int<1>, Int<2>, Int<3> >, Int<2> >::value,
+                   Int<2> >();
+    StaticCheckEQ< List_Ref< List< Int<3> >, Int<2> >::value, Int<3> >();
+
+    typedef List< List< Int<2>, Bool<false> >, 
+                  List< Int<2>, Bool<true> >, 
+                  List< Int<4> > >  
+            L1;
+    StaticCheckEQ< List_Ref< List_Ref< L1, Int<1> >::value, Int<1> >::value, Bool<true> >();
+    StaticCheckEQ< List_Ref< List_Ref< L1, Int<2> >::value, Int<0> >::value, Int<4> >();
+   
     return 0;
 }

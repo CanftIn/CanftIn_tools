b44d55eb989ed75a5fa13715cb4add54e6a2ef1d
CanftIn <wwc7033@gmail.com>
2018-02-18 19:55:20 +0800

    update

--- toys/TemplateLisp/evaluator.hpp
+++ toys/TemplateLisp/evaluator.hpp
@@ -16,143 +16,6 @@ namespace CAN
         struct EmptyType;
         struct Unit;
 
-        // Add
-        template<class T1, class T2> struct Add;
-        template<int N1, int N2>
-        struct Add<Int<N1>, Int<N2>>
-        {
-            using value = Int<(N1 + N2)>;
-        };
-
-        // Compare
-        // >
-        template<class T1, class T2> struct IsGreater;
-        template<int N1, int N2>
-        struct IsGreater<Int<N1>, Int<N2>>
-        {
-            using value = Bool<(N1 > N2)>;
-        };
-
-        // <
-        template<class T1, class T2> struct IsLess;
-        template<int N1, int N2>
-        struct IsLess<Int<N1>, Int<N2>>
-        {
-            using value = Bool<(N1 < N2)>;
-        };
-
-        // ==
-        template<class T1, class T2> struct IsEqual;
-        template<int N1, int N2>
-        struct IsEqual<Int<N1>, Int<N2>>
-        {
-            using value = Bool<(N1 == N2)>;
-        };
-
-        // IsUnit
-        template<class T>
-        struct IsUnit
-        {
-            using value = Bool<false>;
-        };
-        template<>
-        struct IsUnit<Unit>
-        {
-            using value = Bool<true>;
-        };
-
-        // Pair
-        template<class T1, class T2> struct Pair;
-
-        // Pair.first
-        template<class T> struct First;
-        template<class T1, class T2>
-        struct First<Pair<T1, T2>>
-        {
-            using value = T1;
-        };
-
-        // Pair.second
-        template<class T> struct Second;
-        template<class T1, class T2>
-        struct Second<Pair<T1, T2>>
-        {
-            using value = T2;
-        };
-
-        // List
-        template<class T, class... T_Other> 
-        struct List
-        {
-            using value = Pair<T, typename List<T_Other...>::value>;
-        };
-        template<class T>
-        struct List<T>
-        {
-            using value = Pair<T, Unit>;
-        };
-
-        // IsList
-        template<class T>
-        struct IsList
-        {
-            using value = Bool<false>;
-        };
-        template<class T1, class T2>
-        struct IsList<Pair<T1, T2>>
-        {
-            using value = typename IsList<T2>::value;
-        };
-        template<class T>
-        struct IsList<Pair<T, Unit>>
-        {
-            using value = Bool<true>;
-        };
-        template<>
-        struct IsList<Unit>
-        {
-            using value = Bool<true>;
-        };
-
-        // List.N
-        template<class T, class N> struct List_Ref;
-        template<class T1, class T2, int N>
-        struct List_Ref<Pair<T1, T2>, Int<N>>
-        {
-            using value = typename List_Ref<T2, Int<N-1>>::value;
-        };
-        template<class T1, class T2>
-        struct List_Ref<Pair<T1, T2>, Int<0>>
-        {
-            using value = T1;
-        };
-
-        // ListAppend
-        template<class L, class NewT> struct ListAppend;
-        template<class H, class T, class NewT>
-        struct ListAppend<Pair<H, T>, NewT>
-        {
-            using value = Pair<H, typename ListAppend<T, NewT>::value>;
-        };
-        template<class NewT>
-        struct ListAppend<Unit, NewT>
-        {
-            using value = Pair<NewT, Unit>;
-        };
-
-        // If_Then_Else
-        template<class Cond, class T1, class T2> struct If_Then_Else;
-        template<class T1, class T2>
-        struct If_Then_Else<Bool<true>, T1, T2>
-        {
-            using value = T1;
-        };
-        template<class T1, class T2>
-        struct If_Then_Else<Bool<false>, T1, T2>
-        {
-            using value = T2;
-        };
-
         // VarValList
         struct EmptyVarValList;
         template<class Varible, class Val, class Tail> struct VarValList;
@@ -207,6 +70,299 @@ namespace CAN
         {
             using value = EmptyType;
         };
+
+        // Eval
+        template<class Exp, class Environ> struct EvalUnderEnv;
+
+        template<class Exp>
+        struct Eval
+        {
+            using value = typename EvalUnderEnv<Exp, EmptyEnv>::value;
+        };
+
+        template<int N, class Environ>
+        struct EvalUnderEnv<Int<N>, Environ>
+        {
+            using value = Int<N>;
+        };
+        template<bool B, class Environ>
+        struct EvalUnderEnv<Bool<B>, Environ>
+        {
+            using value = Bool<B>;
+        };
+        template<int N, class Environ>
+        struct EvalUnderEnv<Var<N>, Environ>
+        {
+            using value = Var<N>;
+        };
+        template<class Environ>
+        struct EvalUnderEnv<Unit, Environ>
+        {
+            using value = Unit;
+        };
+
+        // Add
+        template<class T1, class T2> struct Add;
+        template<int N1, int N2, class Environ>
+        struct EvalUnderEnv<Add<Int<N1>, Int<N2>>, Environ>
+        {
+            using value = Int<(N1 + N2)>;
+        };
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<Add<T1, T2>, Environ>
+        {
+            using T1Val = typename EvalUnderEnv<T1, Environ>::value;
+            using T2Val = typename EvalUnderEnv<T2, Environ>::value;
+            using value = typename EvalUnderEnv<Add<T1Val, T2Val>, Environ>::value;
+        };
+        
+        // Compare
+        // >
+        template<class T1, class T2> struct IsGreater;
+        template<int N1, int N2, class Environ>
+        struct EvalUnderEnv<IsGreater<Int<N1>, Int<N2>>, Environ>
+        {
+            using value = Bool<(N1 > N2)>;
+        };
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<IsGreater<T1, T2>, Environ>
+        {
+            using T1Val = typename EvalUnderEnv<T1, Environ>::value;
+            using T2Val = typename EvalUnderEnv<T2, Environ>::value;
+            using value = typename EvalUnderEnv<IsGreater<T1Val, T2Val>, Environ>::value;
+        };
+
+        // <
+        template<class T1, class T2> struct IsLess;
+        template<int N1, int N2, class Environ>
+        struct EvalUnderEnv<IsLess<Int<N1>, Int<N2>>, Environ>
+        {
+            using value = Bool<(N1 < N2)>;
+        };
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<IsLess<T1, T2>, Environ>
+        {
+            using T1Val = typename EvalUnderEnv<T1, Environ>::value;
+            using T2Val = typename EvalUnderEnv<T2, Environ>::value;
+            using value = typename EvalUnderEnv<IsLess<T1Val, T2Val>, Environ>::value;
+        };
+
+        // ==
+        template<class T1, class T2> struct IsEqual;
+        template<int N1, int N2, class Environ>
+        struct EvalUnderEnv<IsEqual<Int<N1>, Int<N2>>, Environ>
+        {
+            using value = Bool<(N1 == N2)>;
+        };
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<IsEqual<T1, T2>, Environ>
+        {
+            using T1Val = typename EvalUnderEnv<T1, Environ>::value;
+            using T2Val = typename EvalUnderEnv<T2, Environ>::value;
+            using value = typename EvalUnderEnv<IsEqual<T1Val, T2Val>, Environ>::value;
+        };
+
+        // IsUnit
+        template<class T> struct IsUnit;
+        template<class Environ>
+        struct EvalUnderEnv<IsUnit<Unit>, Environ>
+        {
+            using value = Bool<true>;
+        };
+        template<class T, class Environ>
+        struct EvalUnderEnv<IsUnit<T>, Environ>
+        {
+            using TVal = typename EvalUnderEnv<T, Environ>::value;
+            using value = typename Select< IsEQ<TVal, Unit>::value,
+                                           Bool<true>,
+                                           Bool<false> >::value;
+        };
+
+        // Pair
+        template<class T1, class T2> struct Pair;
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<Pair<T1, T2>, Environ>
+        {
+            using T1Val = typename EvalUnderEnv<T1, Environ>::value;
+            using T2Val = typename EvalUnderEnv<T2, Environ>::value;
+            using value = Pair<T1Val, T2Val>;
+        };
+
+        // Pair.first
+        template<class T> struct First;
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<First<Pair<T1, T2>>, Environ>
+        { 
+            using value = T1; 
+        };
+        template<class T, class Environ>
+        struct EvalUnderEnv<First<T>, Environ>
+        {
+            using TVal = typename EvalUnderEnv<T, Environ>::value;
+            using value = typename EvalUnderEnv<First<TVal>, Environ>::value;
+        };
+
+        // Pair.second
+        template<class T> struct Second;
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<Second<Pair<T1, T2>>, Environ>
+        { 
+            using value = T2; 
+        };
+        template<class T, class Environ>
+        struct EvalUnderEnv<Second<T>, Environ>
+        {
+            using TVal = typename EvalUnderEnv<T, Environ>::value;
+            using value = typename EvalUnderEnv<Second<TVal>, Environ>::value;
+        };
+
+        // List
+        template<class... T_Other> struct List;
+
+        template<class T, class... T_Other, class Environ> 
+        struct EvalUnderEnv<List<T, T_Other...>, Environ>
+        {
+            static const int ArgsNum = sizeof...(T_Other) + 1;
+            using TVal = typename EvalUnderEnv<T, Environ>::value;
+            using TailType = typename Select< ArgsNum == 1,
+                                              Unit,
+                                              List<T_Other...> >::value;
+            using value = Pair<TVal, typename EvalUnderEnv<TailType, Environ>::value>;
+        };
+
+        // IsList
+        template<class T>
+        struct IsList
+        {
+            static const bool MayBeList = false;
+        };
+        template<class T1, class T2>
+        struct IsList<Pair<T1, T2>>
+        {
+            static const bool MayBeList = true;
+        };
+        template<>
+        struct IsList<Unit>
+        {
+            static const bool MayBeList = true;
+        };
+
+        template<class T, class Environ>
+        struct EvalUnderEnv<IsList<T>, Environ>
+        {
+            using TVal = typename EvalUnderEnv<T, Environ>::value;
+            using TmpType = typename Select< IsList<TVal>::MayBeList,
+                                             TVal,
+                                             EmptyType >::value;
+            using value = typename EvalUnderEnv<IsList<TmpType>, Environ>::value;
+        };
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<IsList<Pair<T1, T2>>, Environ>
+        {
+            using T2Val = typename EvalUnderEnv<T2, Environ>::value;
+            using value = typename EvalUnderEnv<IsList<T2Val>, Environ>::value;
+        };
+        template<class T, class Environ>
+        struct EvalUnderEnv<IsList<Pair<T, Unit>>, Environ>
+        {
+            using value = Bool<true>;
+        };
+        template<class Environ>
+        struct EvalUnderEnv<IsList<Unit>, Environ>
+        {
+            using value = Bool<true>;
+        };
+        template<class Environ>
+        struct EvalUnderEnv<IsList<EmptyType>, Environ>
+        {
+            using value = Bool<false>;
+        };
+
+        // List.N
+        template<class T, class N> 
+        struct List_Ref
+        {
+            static const bool MayBeList = false;
+        };
+        template<class T1, class T2, int N>
+        struct List_Ref<Pair<T1, T2>, Int<N>>
+        {
+            static const bool MayBeList = true;
+        };
+
+        template<class T, class N, class Environ>
+        struct EvalUnderEnv<List_Ref<T, N>, Environ>
+        {
+            using TVal = typename EvalUnderEnv<T, Environ>::value;
+            using Num = typename EvalUnderEnv<N, Environ>::value;
+            using TmpType = typename Select< List_Ref<TVal, Num>::MayBeList,
+                                             TVal,
+                                             EmptyType >::value;
+            using value = typename EvalUnderEnv<List_Ref<TmpType, Num>, Environ>::value;
+        };
+        template<class T1, class T2, int N, class Environ>
+        struct EvalUnderEnv<List_Ref<Pair<T1, T2>, Int<N>>, Environ>
+        {
+            using T2Val = typename EvalUnderEnv<T2, Environ>::value;
+            using value = typename EvalUnderEnv<List_Ref<T2Val, Int<N-1>>, Environ>::value;
+        };
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<List_Ref<Pair<T1, T2>, Int<0>>, Environ>
+        {
+            using value = typename EvalUnderEnv<T1, Environ>::value;
+        };
+        template<class N, class Environ>
+        struct EvalUnderEnv<List_Ref<EmptyType, N>, Environ>
+        {
+            using value = Bool<false>;
+        };
+
+        // ListAppend
+        template<class L, class NewT> struct ListAppend;
+        template<class T, class NewT, class Environ>
+        struct EvalUnderEnv<ListAppend<T, NewT>, Environ>
+        {
+            using TVal =  typename EvalUnderEnv<T, Environ>::value;
+            using NewTVal = typename EvalUnderEnv<NewT, Environ>::value;
+            using value = typename EvalUnderEnv<ListAppend<TVal, NewTVal>, Environ>::value;
+        };
+        template<class H, class T, class NewT, class Environ>
+        struct EvalUnderEnv<ListAppend<Pair<H, T>, NewT>, Environ>
+        {
+            using HVal = typename EvalUnderEnv<H, Environ>::value;
+            using TVal = typename EvalUnderEnv<T, Environ>::value;
+            using NewTVal = typename EvalUnderEnv<NewT, Environ>::value;
+            using value = Pair< HVal, 
+                                typename EvalUnderEnv<ListAppend<TVal, NewTVal>, Environ>::value>;
+        };
+        template<class NewT, class Environ>
+        struct EvalUnderEnv<ListAppend<Unit, NewT>, Environ>
+        {
+            using NewTVal = typename EvalUnderEnv<NewT, Environ>::value;
+            using value = Pair<NewTVal, Unit>;
+        };
+
+        // If_Then_Else
+        template<class Cond, class T1, class T2> struct If_Then_Else;
+        template<class Cond, class T1, class T2, class Environ>
+        struct EvalUnderEnv< If_Then_Else<Cond, T1, T2>, Environ >
+        {
+            using CondVal = typename EvalUnderEnv<Cond, Environ>::value;
+            using value = typename EvalUnderEnv<If_Then_Else<CondVal, T1, T2>, Environ>::value;
+        };
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<If_Then_Else<Bool<true>, T1, T2>, Environ>
+        { 
+            using T1Val = typename EvalUnderEnv<T1, Environ>::value;
+            using value = T1Val;
+        };
+        template<class T1, class T2, class Environ>
+        struct EvalUnderEnv<If_Then_Else<Bool<false>, T1, T2>, Environ>
+        {
+            using T2Val = typename EvalUnderEnv<T2, Environ>::value;
+            using value = T2Val;
+        };
+        
     }
 }
 
--- toys/TemplateLisp/evaluator_test.cpp
+++ toys/TemplateLisp/evaluator_test.cpp
@@ -6,60 +6,75 @@ using namespace CAN::TL::UTIL;
 
 int main()
 {
+    // Basic Testing
+    StaticCheckEQ< Eval< Int<4> >, Int<4> >;
+    StaticCheckEQ< Eval< Bool<true> >, Bool<true> >;
+    StaticCheckEQ< Eval< Var<4> >, Var<4> >;
+    StaticCheckEQ< Eval< Unit >, Unit >;
+
     // Add Testing
-    StaticCheckEQ< Add< Int<1>, Int<3> >::value, Int<4> >();
-    StaticCheckEQ< Add< Int<-9>, Int<45> >::value, Int<36> >();
+    StaticCheckEQ< Eval< Add< Add< Int<1>, Int<3> >, Int<4> > >::value, Int<8> >();
+    StaticCheckEQ< Eval< Add< Int<-9>, Int<45> > >::value, Int<36> >();
 
 
     // Comparing Testing
-    StaticCheckEQ< IsGreater< Int<5>, Int<8> >::value, Bool<false> >();
-    StaticCheckEQ< IsLess< Int<5>, Int<8> >::value, Bool<true> >();
-    StaticCheckEQ< IsEqual< Int<8>, Int<8> >::value, Bool<true> >();
+    StaticCheckEQ< Eval< IsGreater< Add< Int<1>, Int<4> >, Int<8> > >::value, Bool<false> >();
+    StaticCheckEQ< Eval< IsLess< Add< Int<1>, Int<4> >, Int<8> > >::value, Bool<true> >();
+    StaticCheckEQ< Eval< IsEqual< Add< Int<4>, Int<4> >, Int<8> > >::value, Bool<true> >();
+
+    StaticCheckEQ< Eval< IsUnit<Unit> >::value, Bool<true> >();
+    StaticCheckEQ< Eval< IsUnit< Add< Int<2>, Int<2> > > >::value, Bool<false> >();
 
     // Pair Testing
-    using P = Pair< Pair<Int<4>, Bool<true>>,
-                    Pair<Pair<Int<2>, Unit>,
-                         Bool<false>>>;
-    StaticCheckEQ< First< First<P>::value >::value, Int<4> >();
-    StaticCheckEQ< Second< First< Second<P>::value >::value >::value, Unit >();
-    StaticCheckEQ< Second< Second<P>::value >::value, Bool<false> >();
+    using P = Pair< Pair< Int<4>, Bool<true> >,
+                    Pair< Pair<Int<2>, Unit>,
+                          Bool<false> > >;
+    StaticCheckEQ< Eval<P>::value, P >();
+    StaticCheckEQ< Eval< First< First<P> > >::value, Int<4> >();
+    StaticCheckEQ< Eval< Second< First< Second<P> > > >::value, Unit >();
+    StaticCheckEQ< Eval< Second< Second<P> > >::value, Bool<false> >();
 
     // List Testing
-    using L1 = List< List< Int<2>, Bool<false> >::value, 
-                     List< Int<4>, Bool<true> >::value, 
-                     List< Int<6> >::value >::value;
+    using L1 = List< List< Int<2>, Bool<false> >, 
+                     List< Int<4>, Bool<true> >, 
+                     List< Int<6> > >;
     using P1 = Pair< Pair< Int<2>, Pair<Bool<false>, Unit> >,
                      Pair< Pair< Int<4>, Pair<Bool<true>, Unit> >,
                            Pair< Pair<Int<6>, Unit>,
                                  Unit > > >;
-    StaticCheckEQ<L1, P1>();
+    StaticCheckEQ< Eval<L1>::value, Eval<P1>::value >();
+    StaticCheckEQ< Eval< List< Int<5> > >::value, Pair< Int<5>, Unit > >();
     // IsList
-    CompileTimeCheck< IsList<L1>::value >();    
-    CompileTimeCheck< IsList<P1>::value >();
-    CompileTimeCheck< IsList<Unit>::value >();
-    CompileTimeCheck< IsList< Second<Second<P1>::value>::value >::value >();
+    StaticCheckEQ< Eval< IsList<L1> >::value, Bool<true> >();
+    StaticCheckEQ< Eval< IsList<P1> >::value, Bool<true> >();
+    StaticCheckEQ< Eval< IsList<Unit> >::value, Bool<true> >();
+    StaticCheckEQ< Eval< IsList< Second<Second<P1> > > >::value, Bool<true> >();
+    StaticCheckEQ< Eval< IsList<P> >::value, Bool<false> >();
     
     // List.N
-    typedef List< Int<0>, Int<1>, Int<2>, Int<3>, Int<4> >::value L3;
-    StaticCheckEQ< List_Ref< L3, Int<2> >::value, Int<2> >();
-    StaticCheckEQ< List_Ref< List_Ref< L1, Int<0> >::value, Int<0> >::value, Int<2> >(); 
+    using L3 = List< Int<0>, Int<1>, Int<2>, Int<3>, Int<4> >;
+    StaticCheckEQ< Eval< List_Ref< L3, Add< Int<2>, Int<0> > > >::value, Int<2> >();
+    StaticCheckEQ< Eval< List_Ref< List_Ref< L1, Add< Int<0>, Int<0> > >, 
+                                  Add< Int<-2>, Int<2> > > >::value, 
+                   Int<2> >(); 
 
     // ListAppend
-    typedef List< Int<0>, Int<1>, Int<2>, Int<3>, Int<4>, Int<5> >::value L4;
-    StaticCheckEQ< ListAppend< L3, Int<5> >::value, L4 >();
-    StaticCheckEQ< ListAppend< ListAppend< L3, Int<5> >::value, List< Int<9> > >, 
-                   ListAppend< L4, List< Int<9> > > >();
+    using L4 = List< Int<0>, Int<1>, Int<2>, Int<3>, Int<4>, Int<5> >;
+    StaticCheckEQ< Eval< ListAppend< L3, Int<5> > >::value, Eval<L4>::value >();
+    StaticCheckEQ< Eval< ListAppend< ListAppend< L3, Add< Int<1>, Int<4> > >, 
+                                     List< Int<9> > > >::value, 
+                   Eval< ListAppend< L4, List< Int<9> > > >::value >();
 
     
     // If_Then_Else Testing
-    StaticCheckEQ< If_Then_Else< IsGreater< Int<5>, Int<8> >::value,
-                                 L1,
-                                 P>::value,
-                   P >();
-    StaticCheckEQ< If_Then_Else< IsLess< Int<5>, Int<8> >::value,
-                                 L1,
-                                 P>::value,
-                   L1 >();    
+    StaticCheckEQ< Eval< If_Then_Else< IsGreater< Int<5>, Int<8> >,
+                                       L1,
+                                       P > >::value,
+                    P >();
+    StaticCheckEQ< Eval< If_Then_Else< IsLess< Int<5>, Int<8> >,
+                                       L1,
+                                       P> >::value,
+                   Eval<L1>::value >();    
 
     // VarValList Testing
     using VarValL0 = VarValList< Var<0>, Int<0>,
